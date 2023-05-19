d5c2bbb585e594e4bac11fdb8be326d1784d78ec
testuser <testuser@test.com>
2023-05-19 16:06:31 +0900

    test class1

---
+++ src/ifelse/TestClass01.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class TestClass01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if (0<num && num<100) {
+			System.out.println("정상");
+		}
+		else {
+			System.out.println("비정상");
+		}
+	}
+}
